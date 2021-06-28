82f6bc57797dc6545bcff7bf14cabd92c713d7c0
Chris Donaldson <chris@digital-labs.co.uk>
2021-06-28 21:20:20 +0100

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://admin-reviti-int-01.underwriteme.co.uk/api";
+// Logging
+app.use(morgan('dev'));
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+    res.send('This is a proxy service which proxies to Billing and Account APIs.');
+ });
+ // Authorization
+app.use('', (req, res, next) => {
+    if (req.headers.authorization) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+ });
+
+ // Proxy endpoints
+app.use('/proxy', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/proxy`]: '',
+    },
+ }));
+
+ // Start the Proxy
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+ });
